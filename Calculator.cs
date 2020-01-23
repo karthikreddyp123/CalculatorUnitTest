aa9d3f2a434cc66e95f163ee8a597d1747285e98
karthikreddyp123 <karthikreddykohli@gmail.com>
2020-01-23 17:36:44 +0530

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        #region Addition
+        // Adds two integers and returns the result
+        /// <summary>
+        /// Adds two integers <paramref name="a"/> and <paramref name="b"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The sum of two integers.
+        /// </returns>
+        /// <exception cref="System.OverflowException">Thrown when one parameter is max 
+        /// and the other is greater than 0.</exception
+        /// <param name="a">An integer.</param>
+        /// <param name="b">An integer.</param>
+
+        public static int Add(int a, int b)
+        {
+            if ((a == int.MaxValue && b > 0) || (b == int.MaxValue && a > 0))
+                throw new System.OverflowException();
+            return a + b;
+        }
+        // Adds two doubles and returns the result
+        /// <summary>
+        /// Adds two doubles <paramref name="a"/> and <paramref name="b"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The sum of two doubles.
+        /// </returns>
+        /// <exception cref="System.OverflowException">Thrown when one parameter is max 
+        /// and the other is greater than 0.</exception>
+        /// <param name="a">A double precision number.</param>
+        /// <param name="b">A double precision number.</param>
+        public static double Add(double a, double b)
+        {
+            // If any parameter is equal to the max value of an integer
+            // and the other is greater than zero
+            if ((a == double.MaxValue && b > 0) || (b == double.MaxValue && a > 0))
+                throw new System.OverflowException();
+
+            return a + b;
+        }
+        #endregion
+        #region Subration
+        // Subtracts an integer from another and returns the result
+        /// <summary>
+        /// Subtracts <paramref name="b"/> from <paramref name="a"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The difference between two integers.
+        /// </returns>
+        /// <param name="a">An integer.</param>
+        /// <param name="b">An integer.</param>
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        // Subtracts a double from another and returns the result
+        /// <summary>
+        /// Subtracts a double <paramref name="b"/> from another double <paramref name="a"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The difference between two doubles.
+        /// </returns>
+        /// <param name="a">A double precision number.</param>
+        /// <param name="b">A double precision number.</param>
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        #endregion
+        #region Multiplication
+        // Multiplies two integers and returns the result
+        /// <summary>
+        /// Multiplies two integers <paramref name="a"/> and <paramref name="b"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The product of two integers.
+        /// </returns>
+        /// <param name="a">An integer.</param>
+        /// <param name="b">An integer.</param>
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        // Multiplies two doubles and returns the result
+        /// <summary>
+        /// Multiplies two doubles <paramref name="a"/> and <paramref name="b"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The product of two doubles.
+        /// </returns>
+        /// <param name="a">A double precision number.</param>
+        /// <param name="b">A double precision number.</param>
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        #endregion
+        #region Division
+        // Divides an integer by another and returns the result
+        /// <summary>
+        /// Divides an integer <paramref name="a"/> by another integer <paramref name="b"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The quotient of two integers.
+        /// </returns>
+        /// <exception cref="System.DivideByZeroException">Thrown when <paramref name="b"/> is equal to 0.</exception>
+        /// <param name="a">An integer dividend.</param>
+        /// <param name="b">An integer divisor.</param>
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        // Divides a double by another and returns the result
+        /// <summary>
+        /// Divides a double <paramref name="a"/> by another double <paramref name="b"/> and returns the result.
+        /// </summary>
+        /// <returns>
+        /// The quotient of two doubles.
+        /// </returns>
+        /// <exception cref="System.DivideByZeroException">Thrown when <paramref name="b"/> is equal to 0.</exception>
+        /// <param name="a">A double precision dividend.</param>
+        /// <param name="b">A double precision divisor.</param>
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+        #endregion
+    }
+}
---
+++ CalculatorTest.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Add_Test()
+        {
+            //Arrange
+            int expected = 10;
+            int num1 = 6;
+            int num2 = 4;
+            //Act
+            int actual = CalculatorLibrary.Calculator.Add(num1, num2);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_Test_FailMessage()
+        {
+            //Arrange
+            int expected = 10;
+            int num1 = 6;
+            int num2 = 6;
+            //Act
+            int actual = CalculatorLibrary.Calculator.Add(num1, num2);
+            //Assert
+            Assert.AreEqual(expected, actual,"Test Case Failed");
+        }
+        [TestMethod]
+        public void Add_Test_Double()
+        {
+            //Arrange
+            double expected = 10.0;
+            double num1 = 6.0;
+            double num2 = 4.0;
+            //Act
+            double actual = CalculatorLibrary.Calculator.Add(num1, num2);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_Test_OverflowException()
+        {
+            //Arrange
+            int num1 = int.MaxValue;
+            int num2 = 4;
+            //Act and Assert
+            Assert.ThrowsException<System.OverflowException>(() => CalculatorLibrary.Calculator.Add(num1, num2));
+        }
+        [TestMethod]
+        public void Add_Test_Double_OverflowException()
+        {
+            //Arrange
+            double num1 = double.MaxValue;
+            double num2 = 4.0;
+            //Act and Assert
+            Assert.ThrowsException<System.OverflowException>(() => CalculatorLibrary.Calculator.Add(num1, num2));
+        }
+        [TestMethod]
+        public void Subtract_Test()
+        {
+            //Arrange
+            int expected = 3;
+            int num1 = 6;
+            int num2 = 4;
+            //Act
+            int actual = CalculatorLibrary.Calculator.Subtract(num1, num2);
+            //Assert
+            Assert.AreEqual(expected, actual,1);
+        }
+        [TestMethod]
+        public void Subtract_Test_Double()
+        {
+            //Arrange
+            double expected = 2.0;
+            double num1 = 6.0;
+            double num2 = 4.0;
+            //Act
+            double actual = CalculatorLibrary.Calculator.Subtract(num1, num2);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Multiply_Test()
+        {
+            //Arrange
+            int expected = 24;
+            int num1 = 6;
+            int num2 = 4;
+            //Act
+            int actual = CalculatorLibrary.Calculator.Multiply(num1, num2);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Multiply_Test_Double()
+        {
+            //Arrange
+            double expected = 24.0;
+            double num1 = 6.0;
+            double num2 = 4.0;
+            //Act
+            double actual = CalculatorLibrary.Calculator.Multiply(num1, num2);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Divide_Test()
+        {
+            //Arrange
+            int expected = 1;
+            int num1 = 6;
+            int num2 = 4;
+            //Act
+            int actual = CalculatorLibrary.Calculator.Divide(num1, num2);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Divide_Test_DivideByZeroException()
+        {
+            //Arrange
+            int num1 = 6;
+            int num2 = 0;
+            //Act and Assert
+            Assert.ThrowsException<System.DivideByZeroException>(() => CalculatorLibrary.Calculator.Divide(num1, num2));
+        }
+        [TestMethod]
+        public void Divide_Test_Double()
+        {
+            //Arrange
+            double expected = 1.5;
+            double num1 = 6.0;
+            double num2 = 4.0;
+            //Act
+            double actual = CalculatorLibrary.Calculator.Divide(num1, num2);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+       
+
+    }
+}
